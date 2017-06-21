9e7c011f672abf8460ef833f4f69172d134c6abf
Ch1b1k0 <baileykanoe@hawaii.rr.com>
2017-06-20 14:57:15 -1000

    Add files via upload

---
+++ hungry-bear/app.js
@@ -0,0 +1,9 @@
+function eatHumans(bearName, goodTastingHumans){
+	//console.log(bearName);
+	//console.log(goodTastingHumans)
+	//var bearName= 
+	//var goodTastingHumans=["Mewmew", "Bob Marley", "Taylor Swift"]
+	console.log(goodTastingHumans[0] + "was eaten by " + bearName + ", " + goodTastingHumans[1] + "was not eaten by " + bearName + ", " + goodTastingHumans[3] + "was eaten by " +bearName) 
+}
+eatHumans("THEAWESOMECHIBIKO", ["Mewmew", "Bob Marley", "Taylor Swift"])
+//eatHumans(bearName, goodTastingHumans);
